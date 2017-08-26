bbd5af064fbff6fa19b0ce52cdf50b941463e1e9
Petter Karlsrud <petterkarlsrud@me.com>
2017-08-26 13:45:30 +0200

    Initial

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace FizzBuzz
+{
+  /// <summary>
+  ///  FizzRunner
+  /// </summary>
+  public class FizzRunner 
+  {
+
+    /// <summary>
+    ///  The amount of rounds you want to run the Fizz Buzz.
+    /// </summary>
+    public int rounds;
+    public string[] results;
+
+    /// <summary>
+    ///  Constructor for the FizzRunner
+    /// </summary>
+    /// <param name="_rounds">Takes the number of rounds you want the FizzRunner to use</param>
+    public FizzRunner(int _rounds) {
+      rounds = _rounds;
+      results = Generate();
+    }
+
+    /// <summary>
+    ///  The function which generates the Fizz Buzz
+    /// </summary>
+    private string[] Generate() {
+      var fizzResults = new string[rounds + 1];
+      for (int i = 1; i <= rounds; i++) {
+        if (i % 15 == 0) {
+          fizzResults[i] = "fizzBuzz";
+        }
+
+        if (i  % 5 == 0) {
+          fizzResults[i] = "buzz"; 
+        }
+
+        if (i % 3 == 0) {
+          fizzResults[i] = "fizz";
+        }
+      }
+      return fizzResults;
+    }
+
+    public void PrintAll () {
+      for (int i = 1; i <= rounds; i++) {
+        Console.WriteLine(i + " " + results[i]);
+      }
+    }
+  }
+}
---
+++ cli/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FizzBuzz;
+
+namespace cli
+{
+  /// <summary>
+  ///  This class is only used to start the init the FizzRunner and run it.
+  /// </summary>
+  class Program 
+  {
+    static void Main(string[] args)
+    {
+      FizzRunner fz = new FizzRunner(100);
+      fz.PrintAll();
+    }
+  }
+}
